fb5b61d80020ca5f1fb08b2e43fe6318744bf3b8
Joseph Kirkish <kirkish.2@gmail.com>
2023-09-20 15:27:41 -0400

    updated view
    updated views. wanted to fix how the error404page results

--- src/main/java/com/coderscampus/flightTrack/web/CustomErrorController.java
+++ src/main/java/com/coderscampus/flightTrack/web/CustomErrorController.java
@@ -1,24 +1,19 @@
 package com.coderscampus.flightTrack.web;
 
-import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class CustomErrorController implements ErrorController {
-
-    @RequestMapping("/error")
-    public String handleError() {
-        return "404error"; // Return the name of the HTML template for the 404 error page
-    }
-
-    
-    public String getErrorPath() {
-        return "/404error";
-    }
+@ControllerAdvice
+public class CustomErrorController {
+	 @ExceptionHandler(HttpClientErrorException.class)
+	    public ModelAndView handleHttpClientErrorException(HttpClientErrorException ex) {
+	        // Create a ModelAndView for the custom error page
+	        ModelAndView modelAndView = new ModelAndView("404error"); // Use the name of your custom HTML file
+	        modelAndView.addObject("errorMessage", "Resource not found (404)");
+	        return modelAndView;
+	    }
 }
 
 
